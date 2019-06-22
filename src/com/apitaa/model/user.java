792907eabc5e0f92178bca29dd0343aedf004086
mhadam-annavi <C:\Users\mhada\git>
2019-06-22 19:47:17 -0400

    Local - First Commit - user.java

---
+++ src/com/apitaa/model/user.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.apitaa.model;
+
+/**
+ * @author mhada
+ *
+ */
+public class user {
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String userName;
+	/**
+	 * 
+	 */
+	public user() {
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
