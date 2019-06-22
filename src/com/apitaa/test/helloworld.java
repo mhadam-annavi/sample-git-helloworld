7b405a2dacf897675d2a33559ee491fd943767ac
mhadam-annavi <C:\Users\mhada\git>
2019-06-22 18:50:40 -0400

    First Commit of Hello World

---
+++ src/com/apitaa/test/helloworld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.apitaa.test;
+
+/**
+ * @author mhada
+ *
+ */
+public class helloworld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hurray!! - Welcome back my friend Adam :-)");
+	}
+
+}
