05f1269d6551aaefed1c9d90b296b152ae352b54
mhadam-annavi <C:\Users\mhada\git>
2019-06-22 18:51:29 -0400

    Added String name

--- src/com/apitaa/test/helloworld.java
+++ src/com/apitaa/test/helloworld.java
@@ -12,6 +12,9 @@ public class helloworld {
 	/**
 	 * @param args
 	 */
+	
+	private String name;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hurray!! - Welcome back my friend Adam :-)");
