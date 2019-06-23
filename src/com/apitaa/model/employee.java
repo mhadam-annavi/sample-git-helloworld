4da95e46dcc86f78c7cb0fd0ecdaffb75f7dd989
mhadam-annavi <C:\Users\mhada\git>
2019-06-22 20:03:02 -0400

    Local - First Commit : Employee

---
+++ src/com/apitaa/model/employee.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.apitaa.model;
+
+/**
+ * @author mhada
+ *
+ */
+public class employee {
+
+	/**
+	 * 
+	 */
+	public employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
