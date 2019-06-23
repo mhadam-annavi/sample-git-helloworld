b9c5be385fcf2106c0d7c3c954fcd479c26589f8
mhadam-annavi <C:\Users\mhada\git>
2019-06-22 20:17:52 -0400

    added get & setter

--- src/com/apitaa/model/employee.java
+++ src/com/apitaa/model/employee.java
@@ -32,6 +32,20 @@ public class employee {
 	public employee() {
 		// TODO Auto-generated constructor stub
 	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 	
 
