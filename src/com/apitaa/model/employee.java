545eb43fe7138904acf976a4a51ac629d36d258e
mhadam-annavi <C:\Users\mhada\git>
2019-06-22 20:04:14 -0400

    added getter & setter

--- src/com/apitaa/model/employee.java
+++ src/com/apitaa/model/employee.java
@@ -12,8 +12,26 @@ public class employee {
 	/**
 	 * 
 	 */
+	public int empid;
+	
+	/**
+	 * @return the empid
+	 */
+	public int getEmpid() {
+		return empid;
+	}
+
+	/**
+	 * @param empid the empid to set
+	 */
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+
 	public employee() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	
 
 }
